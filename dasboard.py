beed50e8e38b4052a7f44ada0c4c09f77aab0c93
apriliasans <apriliasani14@gmail.com>
2024-12-25 14:02:40 +0800

    Update dasboard.py
    all_df = all_df.sort_values(by="order_approved_at", inplace=True)  # Opsional (dijelaskan nanti)

--- dasboard.py
+++ dasboard.py
@@ -50,7 +50,7 @@ def create_rfm_df(df):
 url_raw_github = "https://raw.githubusercontent.com/apriliasans/final_proyek_data_analisis/refs/heads/main/all_data.csv"
 all_df = url_raw_github
 datetime_columns = ["order_approved_at", "order_delivered_customer_date"]
-all_df.sort_values(by="order_approved_at", inplace=True)
+all_df = all_df.sort_values(by="order_approved_at", inplace=True)
 all_df.reset_index(inplace=True)
  
 for column in datetime_columns:
